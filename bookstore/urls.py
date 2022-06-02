c17a841ca1500f86b55ef3ff1f7d870ffc74c8d6
xwojziarnik <woj.ziarnik@gmail.com>
2022-06-02 22:31:01 +0200

    Created superuser, base template, dashboard template and first func.

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import dashboard
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
