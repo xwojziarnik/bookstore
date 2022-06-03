f0a7e82297b80bfee9399bdd1ce62d73c23441a3
xwojziarnik <woj.ziarnik@gmail.com>
2022-06-03 23:01:58 +0200

    Created templates to login and change password. Got some issue when I want to change my password. Django includes admin panel into changing password.

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin, auth
+from django.urls import path, include
 
 from users.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('dashboard/', dashboard, name='dashboard'),
 ]
